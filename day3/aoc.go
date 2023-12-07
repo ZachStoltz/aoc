30d8230c2f22de043f1e7442fa451d5a5ebdc3b8
Zeek <zach@stoltz.io>
2023-12-06 19:06:56 -0500

    Day 3 Part One Complete (Hate that I looked) idk why this was rough

--- day3/aoc.go
+++ day3/aoc.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -19,10 +21,9 @@ func partOne() {
 	}
 	defer file.Close()
 
+	dirs := [][]int{{-1, 0}, {-1,-1}, {0, -1}, {1,-1}, {0, 1}, {1, 1}, {1, 0}, {-1, 1}}
 	scanner := bufio.NewScanner(file)
-
-	digit := solveDayTwoPartOne(0, scanner.Scan(), scanner)
-
+	digit := solveDayThreePartOne([][]string{}, dirs, scanner.Scan(), scanner)
 	finalPrint := fmt.Sprintf("Part One %d\n", digit)
 
 	fmt.Print(finalPrint)
@@ -32,17 +33,106 @@ func partOne() {
 	}
 }
 
-func solveDayTwoPartOne(sum int, hasMoreLines bool, scanner *bufio.Scanner) int {
-	if (!hasMoreLines) {
-		return sum
+func solveDayThreePartOne(matrix [][]string, dirs [][]int, hasMoreLines bool, scanner *bufio.Scanner) int {
+	if (hasMoreLines) {
+		split := strings.Split(scanner.Text(), "")
+
+		fmt.Println(split)
+
+		matrix = append(matrix, split)
+
+		return solveDayThreePartOne(matrix, dirs, scanner.Scan(), scanner)
 	}
 
-	text := scanner.Text()
+	sum := 0
 
-	fmt.Println(text)
+	for y, row := range matrix {
+		isNumber := false;
+    currentNumber := "";
+    check := true
 
-	return solveDayTwoPartOne(0, scanner.Scan(), scanner);
+		for x, char := range row {
+			isNumber = isCharNumber(char)
+
+			if (!isNumber && !check) {
+				fmt.Println(fmt.Sprintf("current Number -> %s", currentNumber))
+
+				curr, _ := strconv.Atoi(currentNumber)
+
+				sum += curr
+			}
+
+			if (!isNumber) {
+				currentNumber = ""
+				check = true
+			}
+
+			if (isNumber && check) {
+				doesPass := false
+
+				for _, dir := range dirs {
+					char, hasValue := get(x, y, dir, matrix)
+
+					doesPass = doesPass || hasValue && !isDot(char) && !isCharNumber(char)
+				}
+
+				if (doesPass) {
+					check = false
+				}
+			}
+
+			if (isNumber) {
+				charNum, _ := get(x, y, []int{0,0}, matrix)
+				currentNumber += charNum
+			}
+		}
+		if (isNumber && !check) {
+			fmt.Println(fmt.Sprintf("current Number -> %s", currentNumber))
+
+			curr, _ := strconv.Atoi(currentNumber)
+
+			sum += curr
+		}
+	}
+
+	return sum;
 }
+
+func isDot(char string) bool {
+	return char == "."
+}
+
+func isCharNumber(char string) bool {
+	_, err := strconv.Atoi(char)
+
+	return err == nil
+}
+
+func get(x int, y int, dir []int, contents [][]string) (string,bool) {
+	xDir := dir[0]
+	yDir := dir[1]
+	dy := y + yDir
+	dx := x + xDir
+
+	// fmt.Println(fmt.Sprintf("xDir %d yDir %d x %d y %d dx %d dy %d", xDir, yDir, x,y,dx,dy))
+
+	if (dy < 0 || dy > len(contents) - 1) {
+		return "" , false
+	}
+
+	chars := contents[dy]
+
+	if (chars == nil) {
+		return "", false
+	}
+
+	if (dx < 0 || dx > len(chars) - 1) {
+		return "", false
+	}
+
+	return chars[dx], true;
+}
+
 
 func partTwo() {
 	file, err := os.Open("/Users/zachstoltz/develop/aoc/day3/data-p2")
@@ -53,7 +143,7 @@ func partTwo() {
 
 	scanner := bufio.NewScanner(file)
 
-	digit := solveDayTwoPartTwo(0, scanner.Scan(), scanner)
+	digit := solveDayThreePartTwo(0, scanner.Scan(), scanner)
 
 	finalPrint := fmt.Sprintf("Part two %d\n", digit)
 
@@ -64,7 +154,7 @@ func partTwo() {
 	}
 }
 
-func solveDayTwoPartTwo(sum int, hasMoreLines bool, scanner *bufio.Scanner) int {
+func solveDayThreePartTwo(sum int, hasMoreLines bool, scanner *bufio.Scanner) int {
 	if (!hasMoreLines) {
 		return sum
 	}
@@ -74,5 +164,5 @@ func solveDayTwoPartTwo(sum int, hasMoreLines bool, scanner *bufio.Scanner) int 
 
 	fmt.Println(text)
 
-	return solveDayTwoPartTwo(0, scanner.Scan(), scanner)
+	return solveDayThreePartTwo(0, scanner.Scan(), scanner)
 }
