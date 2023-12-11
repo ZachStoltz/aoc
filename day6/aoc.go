852e1c5545e24bf58559fd394c61277a08026dc2
Zeek <zach@stoltz.io>
2023-12-11 16:27:08 -0500

    Day 6 P2 Completed

--- day6/aoc.go
+++ day6/aoc.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"regexp"
 	"strconv"
-	"strings"
 )
 
 func main() {
@@ -22,7 +21,7 @@ func partOneAndTwo() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	digit := solve(scanner.Scan(), scanner, [][]int{})
+	digit := solve(scanner.Scan(), scanner, []int{})
 	finalPrint := fmt.Sprintf("Result: %d\n", digit)
 
 	fmt.Print(finalPrint)
@@ -32,46 +31,31 @@ func partOneAndTwo() {
 	}
 }
 
-func solve(hasMoreLines bool, scanner *bufio.Scanner, values [][]int) int {
+func solve(hasMoreLines bool, scanner *bufio.Scanner, values []int) int {
 	if (hasMoreLines) {
 		line := scanner.Text()
 		line = regexp.MustCompile(`\w+:.\s+`).ReplaceAllString(line, "")
-		line = regexp.MustCompile(`\s+`).ReplaceAllString(line, " ")
+		line = regexp.MustCompile(`\s+`).ReplaceAllString(line, "")
 
-		numValues := strings.Split(line, " ")
-		nums := []int{}
+			num, _ := strconv.Atoi(line)
 
-		for _, val := range numValues {
-			num, _ := strconv.Atoi(val)
-			nums = append(nums, num)
-		}
-
-		values = append(values, nums)
+		values = append(values, num)
 
 		return solve(scanner.Scan(), scanner, values)
 	}
 
-	total := 1
+	time := values[0]
+	record := values[1]
 
-	raceTimes := values[0]
-	records := values[1]
+	winners := 0
 
-		for idx, time := range raceTimes {
-			record := records[idx]
-			winners := 0
+	for i := 0; i <= time; i++ {
+		score := i * (time - i)
 
-			for i := 0; i <= time; i++ {
-				score := i * (time - i)
-
-				if (score > record) {
-					winners += 1
-				}
-			}
-
-			if (winners != 0) {
-				total *= winners
-			}
+		if (score > record) {
+			winners += 1
 		}
+	}
 
-	return total
+	return winners
 }
